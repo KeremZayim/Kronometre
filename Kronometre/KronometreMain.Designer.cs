947104892b59c44d694dd6acb6c061c9d257331a
Kerem <1741kerem@gmail.com>
2021-10-30 13:06:13 +0300

    Proje dosyası ekle.

---
+++ Kronometre/KronometreMain.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace Kronometre
+{
+    partial class FormKronometre
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlTitleBar = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnBaslatDurdur = new System.Windows.Forms.Button();
+            this.btnSifirla = new System.Windows.Forms.Button();
+            this.lblSalise = new System.Windows.Forms.Label();
+            this.timerSalise = new System.Windows.Forms.Timer(this.components);
+            this.timerSaniye = new System.Windows.Forms.Timer(this.components);
+            this.timerDakika = new System.Windows.Forms.Timer(this.components);
+            this.timerSaat = new System.Windows.Forms.Timer(this.components);
+            this.lblSaniye = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlTitleBar.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlTitleBar
+            // 
+            this.pnlTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.pnlTitleBar.Controls.Add(this.lblTitle);
+            this.pnlTitleBar.Controls.Add(this.btnExit);
+            this.pnlTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTitleBar.Location = new System.Drawing.Point(0, 0);
+            this.pnlTitleBar.Name = "pnlTitleBar";
+            this.pnlTitleBar.Size = new System.Drawing.Size(331, 32);
+            this.pnlTitleBar.TabIndex = 0;
+            this.pnlTitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TasimaIslemi);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Aquire", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
+            this.lblTitle.Location = new System.Drawing.Point(10, 6);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(116, 16);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Kronometre";
+            this.lblTitle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TasimaIslemi);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Transparent;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(70)))));
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Aquire", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(3)));
+            this.btnExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
+            this.btnExit.Location = new System.Drawing.Point(299, 0);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(32, 32);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "X";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnBaslatDurdur
+            // 
+            this.btnBaslatDurdur.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(62)))), ((int)(((byte)(64)))));
+            this.btnBaslatDurdur.FlatAppearance.BorderSize = 0;
+            this.btnBaslatDurdur.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(70)))));
+            this.btnBaslatDurdur.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBaslatDurdur.Font = new System.Drawing.Font("Aquire", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBaslatDurdur.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
+            this.btnBaslatDurdur.Location = new System.Drawing.Point(51, 363);
+            this.btnBaslatDurdur.Name = "btnBaslatDurdur";
+            this.btnBaslatDurdur.Size = new System.Drawing.Size(106, 53);
+            this.btnBaslatDurdur.TabIndex = 1;
+            this.btnBaslatDurdur.Text = "Baslat";
+            this.btnBaslatDurdur.UseVisualStyleBackColor = false;
+            this.btnBaslatDurdur.Click += new System.EventHandler(this.btnBaslatDurdur_Click);
+            // 
+            // btnSifirla
+            // 
+            this.btnSifirla.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(62)))), ((int)(((byte)(64)))));
+            this.btnSifirla.FlatAppearance.BorderSize = 0;
+            this.btnSifirla.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(70)))));
+            this.btnSifirla.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSifirla.Font = new System.Drawing.Font("Aquire", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSifirla.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
+            this.btnSifirla.Location = new System.Drawing.Point(172, 363);
+            this.btnSifirla.Name = "btnSifirla";
+            this.btnSifirla.Size = new System.Drawing.Size(106, 53);
+            this.btnSifirla.TabIndex = 2;
+            this.btnSifirla.Text = "Sifirla";
+            this.btnSifirla.UseVisualStyleBackColor = false;
+            this.btnSifirla.Click += new System.EventHandler(this.btnSifirla_Click);
+            // 
+            // lblSalise
+            // 
+            this.lblSalise.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSalise.Font = new System.Drawing.Font("Aquire", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalise.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
+            this.lblSalise.Location = new System.Drawing.Point(238, 119);
+            this.lblSalise.Name = "lblSalise";
+            this.lblSalise.Size = new System.Drawing.Size(57, 32);
+            this.lblSalise.TabIndex = 3;
+            this.lblSalise.Text = "00";
+            this.lblSalise.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // timerSalise
+            // 
+            this.timerSalise.Interval = 10;
+            this.timerSalise.Tick += new System.EventHandler(this.timerSalise_Tick);
+            // 
+            // timerSaniye
+            // 
+            this.timerSaniye.Interval = 1000;
+            this.timerSaniye.Tick += new System.EventHandler(this.timerSaniye_Tick);
+            // 
+            // timerDakika
+            // 
+            this.timerDakika.Interval = 60000;
+            this.timerDakika.Tick += new System.EventHandler(this.timerDakika_Tick);
+            // 
+            // timerSaat
+            // 
+            this.timerSaat.Tick += new System.EventHandler(this.timerSaat_Tick);
+            // 
+            // lblSaniye
+            // 
+            this.lblSaniye.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSaniye.AutoSize = true;
+            this.lblSaniye.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblSaniye.Font = new System.Drawing.Font("Aquire", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaniye.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(199)))), ((int)(((byte)(199)))));
+            this.lblSaniye.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.lblSaniye.Location = new System.Drawing.Point(157, 109);
+            this.lblSaniye.Name = "lblSaniye";
+            this.lblSaniye.Size = new System.Drawing.Size(75, 43);
+            this.lblSaniye.TabIndex = 3;
+            this.lblSaniye.Text = "00";
+            this.lblSaniye.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblSalise);
+            this.panel1.Controls.Add(this.lblSaniye);
+            this.panel1.Location = new System.Drawing.Point(24, 106);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(295, 152);
+            this.panel1.TabIndex = 4;
+            // 
+            // FormKronometre
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.ClientSize = new System.Drawing.Size(331, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnSifirla);
+            this.Controls.Add(this.btnBaslatDurdur);
+            this.Controls.Add(this.pnlTitleBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormKronometre";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormKronometre_Load);
+            this.pnlTitleBar.ResumeLayout(false);
+            this.pnlTitleBar.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlTitleBar;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnBaslatDurdur;
+        private System.Windows.Forms.Button btnSifirla;
+        private System.Windows.Forms.Label lblSalise;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Timer timerSalise;
+        private System.Windows.Forms.Timer timerSaniye;
+        private System.Windows.Forms.Timer timerDakika;
+        private System.Windows.Forms.Timer timerSaat;
+        private System.Windows.Forms.Label lblSaniye;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Kronometre/KronometreMain.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kronometre
+{
+    public partial class FormKronometre : Form
+    {
+        //Tasima Islemi
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        //
+        public FormKronometre()
+        {
+            InitializeComponent();
+        }
+
+        private void FormKronometre_Load(object sender, EventArgs e)
+        {
+            btnBaslatDurdur.Text = "Baslat";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void TasimaIslemi(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        int salise = 0, saniye = 0, dakika = 0, saat = 0;
+        private void timerSaat_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timerDakika_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timerSaniye_Tick(object sender, EventArgs e)
+        {
+            saniye++;
+            lblSaniye.Text = Convert.ToString(saniye);
+            if (saniye == 60)
+            {
+                saniye = 0;
+            }
+        }
+
+        private void timerSalise_Tick(object sender, EventArgs e)
+        {
+            salise++;
+            lblSalise.Text = Convert.ToString(salise);
+            if (salise == 100)
+            {
+                salise = 0;
+            }
+        }
+
+        private void btnBaslatDurdur_Click(object sender, EventArgs e)
+        {
+            if (btnBaslatDurdur.Text == "Baslat")
+            {
+                btnBaslatDurdur.Text = "Durdur";
+                timerSalise.Enabled = true;
+                timerSaniye.Enabled = true;
+                timerDakika.Enabled = true;
+                timerSaat.Enabled = true;
+
+
+            }
+            else if (btnBaslatDurdur.Text == "Durdur")
+            {
+                btnBaslatDurdur.Text = "Baslat";
+                timerSalise.Enabled = false;
+                timerSaniye.Enabled = false;
+                timerDakika.Enabled = false;
+                timerSaat.Enabled = false;
+            }
+        }
+
+        private void btnSifirla_Click(object sender, EventArgs e)
+        {
+            salise = 0;
+            saniye = 0;
+            dakika = 0;
+            saat = 0;
+        }
+    }
+}
